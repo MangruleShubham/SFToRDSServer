b7c6a238d258b72c0b0e79c5aa998ff97da7a0b7
shubham <shubhammangrule5@gmail.com>
2024-03-08 13:34:02 +0530

    printing parsing req with comma

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ conn.login(SF_USERNAME,SF_PASSWORD+SF_TOKEN,(err,userInfo)=>{
 
 // sslServer.listen(3001,()=>console.log('Secure server on port 3001'));
 app.post('/',(req,resp)=>{
-    console.log("Hello",req);
-    console.log("Hello",req.body.Id);
+    console.log("Hello",req.query.name);
+    console.log("Hello",req.params.Id);
     return resp.send({body:req.body,message:"Salesforce integration with nodejs"});
 })
 app.listen(PORT,(err)=>{

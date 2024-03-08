0d28954c509dc1174e1d2e5c580a065ec158e509
shubham <shubhammangrule5@gmail.com>
2024-03-08 09:59:27 +0530

    started default path

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ conn.login(SF_USERNAME,SF_PASSWORD+SF_TOKEN,(err,userInfo)=>{
 // },app)
 
 // sslServer.listen(3001,()=>console.log('Secure server on port 3001'));
-// app.post('/',(req,resp)=>{
-//     resp.send({message:"Salesforce integration with nodejs"});
-// })
+app.post('/',(req,resp)=>{
+    resp.send({message:"Salesforce integration with nodejs"});
+})
 app.listen(PORT,(err)=>{
     if(err)console.log(err);
     else
